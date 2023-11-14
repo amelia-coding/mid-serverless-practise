de5f3fc40d698e6da625bb804230a515058427bc
dujuan <dujuan@bixin.cn>
2023-11-14 15:14:41 +0800

    使用eggjs创建简单的应用

---
+++ eggjs-easy/app/controller/home.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const { Controller } = require('egg');
+
+class HomeController extends Controller {
+  async index() {
+    const { ctx } = this;
+    ctx.body = 'hi, egg';
+  }
+
+  async list() {
+    const dataList = {
+      list: [
+        { id: 1, title: 'this is news 1', url: '/news/1' },
+        { id: 2, title: 'this is news 2', url: '/news/2' },
+      ],
+    };
+    await this.ctx.render('list.tpl', dataList);
+  }
+}
+
+module.exports = HomeController;
---
+++ eggjs-framework/README.md
@@ -0,0 +1,25 @@
+# eggjs-framework
+
+## QuickStart
+
+```bash
+$ npm install
+$ npm test
+```
+
+publish your framework to npm, then change app's framework config:
+
+```js
+// {app_root}/index.js
+{
+  "name": "egg-showcase",
+  "egg": {
+    "framework": "eggjs-framework"
+  }
+}
+```
+
+## Questions & Suggestions
+
+Please open an issue [here](https://github.com/eggjs/egg/issues).
+
